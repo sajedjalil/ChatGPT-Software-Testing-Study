202dc70b638699acf8891ae4283361f9def919b3
Sajed Jalil <sjalil@gmu.edu>
2023-01-16 17:22:06 +0600

    Shared context iteration 3 completed.

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,7 @@ from caller import *
 from constant import Constant
 
 if __name__ == '__main__':
-    Constant.iterations = 5
+    Constant.iterations = 3
     Constant.interval_delay = 10  # sec
 
     generate_shared_context()
